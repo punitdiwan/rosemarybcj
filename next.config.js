913c0863c4e64c7f148c42665e7bb1aed42b0622
Punit Diwan <punitdiwan@gmail.com>
2026-03-12 11:59:48 +0530

    Optimize the Code for cost Saving

--- next.config.js
+++ next.config.js
@@ -1,3 +1,26 @@
 module.exports = {
   reactStrictMode: true,
+  compress: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    domains: ['rosemarydn.com'],
+  },
+  experimental: {
+    optimizeCss: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/:all*(jpg|jpeg|png|webp|avif|svg|ico|js|css|woff|woff2)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
 }
